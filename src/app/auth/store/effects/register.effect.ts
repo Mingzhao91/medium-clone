13cb13a8311a65d9261be138a5cb61c4dba71dd7
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-20 23:07:40 +0100

    store token and navigate to home page using effect

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, switchMap, of, map } from 'rxjs';
+import { catchError, switchMap, of, map, tap } from 'rxjs';
 
 import { AuthService } from '../../services/auth.service';
 import {
@@ -10,10 +11,16 @@ import {
   registerSuccessAction,
 } from '../actions/register.actions';
 import { CurrentUserInterface } from '../../../shared/types/currentUser.interface';
+import { PersistanceService } from 'src/app/shared/services/persistance.service';
 
 @Injectable()
 export class RegisterEffect {
-  constructor(private actions$: Actions, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private actions$: Actions,
+    private authService: AuthService,
+    private persistentService: PersistanceService
+  ) {}
 
   register$ = createEffect(() =>
     this.actions$.pipe(
@@ -21,6 +28,7 @@ export class RegisterEffect {
       switchMap(({ request }) => {
         return this.authService.register(request).pipe(
           map((currentUser: CurrentUserInterface) => {
+            this.persistentService.set('accessToken', currentUser.token);
             return registerSuccessAction({ currentUser });
           }),
           catchError((errorResponse: HttpErrorResponse) => {
@@ -31,5 +39,16 @@ export class RegisterEffect {
         );
       })
     )
+  );
+
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/');
+        })
+      ),
+    { dispatch: false }
   );
 }
