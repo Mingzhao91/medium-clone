691981b9378807cea9fdcd8ca8c1e64e00ce342d
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-20 22:03:54 +0100

    listen to register success and failure actions

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,16 +1,45 @@
 import { createReducer, on } from '@ngrx/store';
 
 import { AuthStateInterface } from '../types/authState.interface';
-import { registerAction } from './actions/register.actions';
+import {
+  registerAction,
+  registerFailureAction,
+  registerSuccessAction,
+} from './actions/register.actions';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  currentUser: null,
+  isLoggedIn: null,
+  validationError: null,
 };
 
 export const authReducer = createReducer(
   initialState,
   on(
     registerAction,
-    (state): AuthStateInterface => ({ ...state, isSubmitting: true })
+    (state): AuthStateInterface => ({
+      ...state,
+      isSubmitting: true,
+      validationError: null,
+    })
+  ),
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser,
+    })
+  ),
+  on(
+    registerFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: false,
+      validationError: action.errors,
+    })
   )
 );
