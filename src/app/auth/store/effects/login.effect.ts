e61b2a6749f39c592f180544ada2b619532c7a7e
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-21 15:44:29 +0100

    create login effects

---
+++ src/app/auth/store/effects/login.effect.ts
@@ -0,0 +1,48 @@
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
+
+import {
+  loginAction,
+  loginFailureAction,
+  loginSuccessAction,
+} from '../actions/login.actions';
+import { CurrentUserInterface } from '../../../shared/types/current-user.interface';
+import { AuthService } from '../../services/auth.service';
+import { PersistanceService } from '../../../shared/services/persistance.service';
+
+export class loginEffects {
+  constructor(
+    private router: Router,
+    private actions$: Actions,
+    private authService: AuthService,
+    private persistanceService: PersistanceService
+  ) {}
+
+  login$ = this.actions$.pipe(
+    ofType(loginAction),
+    switchMap(({ request }) => {
+      return this.authService.login(request).pipe(
+        map((currentUser: CurrentUserInterface) => {
+          this.persistanceService.set('accessToken', currentUser.token);
+          return loginSuccessAction({ currentUser });
+        }),
+        catchError((errorResponse: HttpErrorResponse) => {
+          return of(loginFailureAction(errorResponse.error.errors));
+        })
+      );
+    })
+  );
+
+  redirectAfterSubmit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loginSuccessAction),
+        tap(() => {
+          this.router.navigateByUrl('/');
+        })
+      ),
+    { dispatch: false }
+  );
+}
