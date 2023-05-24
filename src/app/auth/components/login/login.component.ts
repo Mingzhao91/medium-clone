838eb4b736a5f90a1f300448b8da215005094157
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-24 21:08:17 +0100

    create login component

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Store, select } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
+
+import {
+  isSumbittingSelector,
+  validationErrorsSelector,
+} from '../../store/selector';
+
+import { BackendErrorMessagesComponent } from '../../../shared/modules/backend-error-messages/components/backend-error-messages/backend-error-messages.component';
+import { LoginRequestInterface } from '../../types/login-request.interface';
+import { loginAction } from '../../store/actions/login.actions';
+
+@Component({
+  selector: 'mc-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    CommonModule,
+    BackendErrorMessagesComponent,
+  ],
+})
+export class LoginComponent {
+  form: FormGroup = this.fb.nonNullable.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  data$ = combineLatest({
+    isSubmitting: this.store.pipe(select(isSumbittingSelector)),
+    backendErrors: this.store.pipe(select(validationErrorsSelector)),
+  });
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+
+  onSubmit(): void {
+    const request: LoginRequestInterface = {
+      user: this.form.getRawValue(),
+    };
+
+    this.store.dispatch(loginAction({ request }));
+  }
+}
