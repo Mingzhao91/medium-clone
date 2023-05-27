8c915bd512cbb3f2a1278abf5d3c0c551de5c00d
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-27 11:26:42 +0100

    show topbar inside the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TopbarComponent } from './shared/components/topbar/topbar.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopbarComponent],
 })
 export class AppComponent {}
