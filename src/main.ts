a754a22dd6e4f95ecc847284c7e4c58dc6e3cc57
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-28 14:48:46 +0100

    add token to header for http requests using interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
+
 import { provideEffects } from '@ngrx/effects';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { authFeatureKey, authReducer } from './app/auth/store/reducers';
 import * as authEffects from './app/auth/store/effects';
-import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import { authInterceptor } from './app/shared/services/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(appRoutes),
     provideStore({
       router: routerReducer,
