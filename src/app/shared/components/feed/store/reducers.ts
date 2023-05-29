006aaa3315e9cd94633d40fb21869f6652bd4376
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-05-29 16:01:19 +0100

    create feature for feed

---
+++ src/app/shared/components/feed/store/reducers.ts
@@ -0,0 +1,38 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { feedActions } from './actions';
+import { routerNavigationAction } from '@ngrx/router-store';
+
+const initState = {
+  isLoading: false,
+  data: null,
+  error: null,
+};
+
+const feedFeature = createFeature({
+  name: 'feed',
+  reducer: createReducer(
+    initState,
+    on(feedActions.getFeed, (state) => ({
+      ...state,
+      isLoading: true,
+    })),
+    on(feedActions.getFeedSuccess, (state, action) => ({
+      ...state,
+      isLoading: false,
+      data: action.feed,
+    })),
+    on(feedActions.getFeedFailure, (state) => ({
+      ...state,
+      isLoading: false,
+    })),
+    on(routerNavigationAction, () => initState)
+  ),
+});
+
+export const {
+  name: feedFeatureKey,
+  reducer: feedReducer,
+  selectIsLoading,
+  selectData: selectFeedData,
+  selectError,
+} = feedFeature;
