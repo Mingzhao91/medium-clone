cb660d70d8a189e7b38ef6d231564049fe722c40
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-03 09:23:32 +0100

    show banner component in global feed page

--- src/app/globalFeed/components/global-feed/global-feed.component.ts
+++ src/app/globalFeed/components/global-feed/global-feed.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeedComponent } from '../../../shared/components/feed/feed.component';
+import { BannerComponent } from '../../../shared/components/banner/banner.component';
 
 @Component({
   selector: 'mc-global-feed',
   standalone: true,
-  imports: [CommonModule, FeedComponent],
+  imports: [CommonModule, FeedComponent, BannerComponent],
   templateUrl: './global-feed.component.html',
 })
 export class GlobalFeedComponent {
