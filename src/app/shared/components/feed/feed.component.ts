8b496a69b427463a18e186e6477daa7016a5fde4
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-03 14:37:54 +0100

    use pagination component in feeds page

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Params, Router, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { feedActions } from './store/actions';
 import { combineLatest } from 'rxjs';
@@ -8,11 +8,19 @@ import { combineLatest } from 'rxjs';
 import { selectError, selectFeedData, selectIsLoading } from './store/reducers';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { LoadingComponent } from '../loading/loading.component';
+import { environment } from '../../../../environments/environment';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'mc-feed',
   standalone: true,
-  imports: [CommonModule, RouterLink, ErrorMessageComponent, LoadingComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ErrorMessageComponent,
+    LoadingComponent,
+    PaginationComponent,
+  ],
   templateUrl: './feed.component.html',
 })
 export class FeedComponent implements OnInit {
@@ -23,10 +31,24 @@ export class FeedComponent implements OnInit {
     error: this.store.select(selectError),
     feed: this.store.select(selectFeedData),
   });
+  limit = environment.limit;
+  baseUrl = this.router.url.split('?')[0];
+  currentPage = 0;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.currentPage = Number(queryParams['page'] || '1');
+      this.fetchFeed();
+    });
+  }
+
+  fetchFeed(): void {
     this.store.dispatch(feedActions.getFeed({ url: this.apiUrl }));
   }
 }
