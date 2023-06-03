38c2aa9727bd22b3a0eb63b3198d85fe9e11e1b3
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-03 16:55:42 +0100

    show popular tags in global feed page

--- src/app/globalFeed/components/global-feed/global-feed.component.ts
+++ src/app/globalFeed/components/global-feed/global-feed.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { FeedComponent } from '../../../shared/components/feed/feed.component';
 import { BannerComponent } from '../../../shared/components/banner/banner.component';
+import { PopularTagsComponent } from '../../../shared/components/popular-tags/popular-tags.component';
 
 @Component({
   selector: 'mc-global-feed',
   standalone: true,
-  imports: [CommonModule, FeedComponent, BannerComponent],
+  imports: [CommonModule, FeedComponent, BannerComponent, PopularTagsComponent],
   templateUrl: './global-feed.component.html',
 })
 export class GlobalFeedComponent {
