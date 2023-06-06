258aa27778a460e5a9a79bdb2d6e859045de1382
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-06 21:22:17 +0100

    create article component

---
+++ src/app/article/components/article/article.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-article',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './article.component.html',
+})
+export class ArticleComponent {}
