957d7bc86263456a277e5283002231c21d3f8f5f
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-12 16:25:08 +0100

    create effects related to article deletion

--- src/app/article/store/effects.ts
+++ src/app/article/store/effects.ts
@@ -1,12 +1,14 @@
 import { inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 
 import { ArticleService as SharedArticleService } from '../../shared/services/article.service';
 import { articleActions } from './actions';
 import { ArticleInterface } from '../../shared/types/article.interface';
+import { ArticleService } from '../services/article.service';
+import { Router } from '@angular/router';
 
-export const articleEffect = createEffect(
+export const getArticleEffect = createEffect(
   (actions$ = inject(Actions), articleService = inject(SharedArticleService)) =>
     actions$.pipe(
       ofType(articleActions.getArticle),
@@ -20,4 +22,29 @@ export const articleEffect = createEffect(
       )
     ),
   { functional: true }
+);
+
+export const deleteArticleEffect = createEffect(
+  (actions$ = inject(Actions), articleService = inject(ArticleService)) =>
+    actions$.pipe(
+      ofType(articleActions.deleteArticle),
+      switchMap(({ slug }) =>
+        articleService.deleteArticle(slug).pipe(
+          map(() => articleActions.deleteArticleSuccess()),
+          catchError(() => of(articleActions.deleteArticleFailure()))
+        )
+      )
+    ),
+  { functional: true }
+);
+
+export const redirectAfterDeleteEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) =>
+    actions$.pipe(
+      ofType(articleActions.deleteArticleSuccess),
+      tap(() => {
+        router.navigateByUrl('/');
+      })
+    ),
+  { functional: true, dispatch: false }
 );
