42437c7266e8eaadfa75090729ca858e08ffb6e7
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-12 14:30:49 +0100

    import inject rathen than Inject from angular core

--- src/app/article/store/effects.ts
+++ src/app/article/store/effects.ts
@@ -1,4 +1,4 @@
-import { Inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, map, of, switchMap } from 'rxjs';
 
@@ -7,7 +7,7 @@ import { articleActions } from './actions';
 import { ArticleInterface } from '../../shared/types/article.interface';
 
 export const articleEffect = createEffect(
-  (actions$ = Inject(Actions), articleService = Inject(SharedArticleService)) =>
+  (actions$ = inject(Actions), articleService = inject(SharedArticleService)) =>
     actions$.pipe(
       ofType(articleActions.getArticle),
       switchMap(({ slug }) =>
