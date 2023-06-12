616df61b9940f9e4197ab02435c49601bd9910fc
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-12 16:57:29 +0100

    init create-article component

---
+++ src/app/create-article/components/create-article.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'mc-create-article',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './create-article.component.html',
+})
+export class CreateArticleComponent {}
