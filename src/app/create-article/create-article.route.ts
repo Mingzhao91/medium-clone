498b79131a4399f26ab01f6f8cca2798c18f5ffe
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-14 16:35:43 +0100

    provide create-article store

--- src/app/create-article/create-article.route.ts
+++ src/app/create-article/create-article.route.ts
@@ -1,12 +1,23 @@
 import { Route } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideState } from '@ngrx/store';
 
 import { CreateArticleComponent } from './components/create-article.component';
 import { CreateArticleService } from './services/create-article.service';
+import * as createArticleEffects from './store/effects';
+import {
+  createArticleFeatureKey,
+  createArticleReducer,
+} from './store/reducers';
 
 export const route: Route[] = [
   {
     path: '',
     component: CreateArticleComponent,
-    providers: [CreateArticleService],
+    providers: [
+      CreateArticleService,
+      provideEffects(createArticleEffects),
+      provideState(createArticleFeatureKey, createArticleReducer),
+    ],
   },
 ];
