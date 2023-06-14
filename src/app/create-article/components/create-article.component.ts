7386e9b4db878449ac27a1b7ce61ae33b98a4ae0
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-14 15:08:59 +0100

    show article form in create article component

--- src/app/create-article/components/create-article.component.ts
+++ src/app/create-article/components/create-article.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ArticleFormValuesInterface } from '../../shared/components/article-form/types/article-form-values.interface';
+import { ArticleFormComponent } from '../../shared/components/article-form/article-form.component';
+
 @Component({
   selector: 'mc-create-article',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ArticleFormComponent],
   templateUrl: './create-article.component.html',
 })
-export class CreateArticleComponent {}
+export class CreateArticleComponent {
+  initialValues = {
+    title: '',
+    description: '',
+    body: '',
+    tagList: [],
+  };
+
+  onSubmit(articleFormValues: ArticleFormValuesInterface): void {
+    console.log('onSubmit in create article', articleFormValues);
+  }
+}
