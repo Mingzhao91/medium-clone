d7e29a2040ebb00cfefdc523e219f5ecf071b424
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-14 18:59:03 +0100

    export edit-article with providers

---
+++ src/app/edit-article/edit-article.route.ts
@@ -0,0 +1,20 @@
+import { Route } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideState } from '@ngrx/store';
+
+import { EditArticleComponent } from './components/edit-article.component';
+import { EditArticleService } from './services/edit-article.service';
+import * as editArticleEffects from './store/effects';
+import { editArticleFeatureKey, editArticleReducer } from './store/reducers';
+
+export const route: Route[] = [
+  {
+    path: '',
+    component: EditArticleComponent,
+    providers: [
+      EditArticleService,
+      provideEffects(editArticleEffects),
+      provideState(editArticleFeatureKey, editArticleReducer),
+    ],
+  },
+];
