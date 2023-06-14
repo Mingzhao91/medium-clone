0708d4430a9e513b00d288074e9f7aba5f7a6f2d
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-14 18:58:15 +0100

    create a service to update an article

---
+++ src/app/edit-article/services/edit-article.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { ArticleRequestInterface } from '../../shared/types/article-request';
+import { ArticleInterface } from '../../shared/types/article.interface';
+
+@Injectable()
+export class EditArticleService {
+  constructor(private http: HttpClient) {}
+
+  updateArticle(
+    slug: string,
+    articleRequest: ArticleRequestInterface
+  ): Observable<ArticleInterface> {
+    const fullUrl = `${environment.apiUrl}/articles/${slug}`;
+    return this.http
+      .put<ArticleRequestInterface>(fullUrl, articleRequest)
+      .pipe(map((response) => response.article));
+  }
+}
