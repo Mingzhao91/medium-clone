496d1b96aeb160a0e025697b39b8bf256f494c71
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-16 15:44:04 +0100

    create method to update user settings

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,6 +7,7 @@ import { CurrentUserInterface } from '../../shared/types/current-user.interface'
 import { RegisterRequestInterface } from '../types/register-request.interface';
 import { AuthResponseInterface } from '../types/auth-response.interface';
 import { LoginRequestInterface } from '../types/login-request.interface';
+import { CurrentUserRequestInterface } from 'src/app/shared/types/current-user-request';
 
 @Injectable({
   providedIn: 'root',
@@ -34,6 +35,15 @@ export class AuthService {
     const url = `${environment.apiUrl}/users/login`;
     return this.http
       .post<AuthResponseInterface>(url, data)
+      .pipe(map(this.getUser));
+  }
+
+  updateCurrentUser(
+    currentUserRequest: CurrentUserRequestInterface
+  ): Observable<CurrentUserInterface> {
+    const url = `${environment.apiUrl}/user`;
+    return this.http
+      .put<AuthResponseInterface>(url, currentUserRequest)
       .pipe(map(this.getUser));
   }
 }
