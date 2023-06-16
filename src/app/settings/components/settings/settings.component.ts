fd68cd461ee3afd23ceded066a808ede41f77c0a
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-16 16:40:10 +0100

    implement settings component

--- src/app/settings/components/settings/settings.component.ts
+++ src/app/settings/components/settings/settings.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
+import { Subscription, combineLatest, filter } from 'rxjs';
+
+import { selectCurrentUser } from '../../../auth/store/reducers';
+import { CurrentUserInterface } from '../../../shared/types/current-user.interface';
+import {
+  selectIsSubmitting,
+  selectValidationErrors,
+} from '../../store/reducers';
+import { BackendErrorMessagesComponent } from '../../../shared/components/backend-error-messages/backend-error-messages.component';
+import { CurrentUserRequestInterface } from '../../../shared/types/current-user-request';
+import { authActions } from '../../../auth/store/actions';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, BackendErrorMessagesComponent],
   templateUrl: './settings.component.html',
 })
-export class SettingsComponent {}
+export class SettingsComponent implements OnInit, OnDestroy {
+  form = this.fb.nonNullable.group({
+    image: '',
+    username: '',
+    bio: '',
+    email: '',
+    password: '',
+  });
+  currentUser?: CurrentUserInterface;
+  data$ = combineLatest({
+    isSubmitting: this.store.select(selectIsSubmitting),
+    backendErrors: this.store.select(selectValidationErrors),
+  });
+  currentUserSubscription?: Subscription;
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+
+  ngOnInit(): void {
+    this.currentUserSubscription = this.store
+      .pipe(select(selectCurrentUser), filter(Boolean))
+      .subscribe((currentUser) => {
+        this.currentUser = currentUser;
+        this.initaliseForm();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.currentUserSubscription?.unsubscribe();
+  }
+
+  initaliseForm(): void {
+    if (!this.currentUser) {
+      throw new Error('Current user is not set');
+    }
+
+    this.form.patchValue({
+      image: this.currentUser.image ?? '',
+      username: this.currentUser.username,
+      bio: this.currentUser.bio ?? '',
+      email: this.currentUser.email,
+      password: '',
+    });
+  }
+
+  submit() {
+    if (!this.currentUser) {
+      throw new Error('Current user is not set');
+    }
+    const currentUserRequest: CurrentUserRequestInterface = {
+      user: {
+        ...this.currentUser,
+        ...this.form.getRawValue(),
+      },
+    };
+
+    this.store.dispatch(authActions.updateCurrentUser({ currentUserRequest }));
+  }
+
+  logout(): void {
+    this.store.dispatch(authActions.logout());
+  }
+}
