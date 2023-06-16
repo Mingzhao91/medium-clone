ee4cad2ecba97c812ab170af2d6e9bd2a52abbec
Mingzhao Zhou <edwardwhite7@hotmail.com>
2023-06-16 21:43:22 +0100

    implement user profile component

--- src/app/user-profile/components/user-profile/user-profile.component.ts
+++ src/app/user-profile/components/user-profile/user-profile.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  ActivatedRoute,
+  Params,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { Store, select } from '@ngrx/store';
+import { combineLatest, filter, map } from 'rxjs';
+import {
+  selectError,
+  selectIsLoading,
+  selectUserProfileData,
+} from '../../store/reducers';
+import { userProfileActions } from '../../store/actions';
+import { selectCurrentUser } from '../../../auth/store/reducers';
+import { UserProfileInterface } from '../../types/user-profile.interface';
+import { CurrentUserInterface } from '../../../shared/types/current-user.interface';
+import { FeedComponent } from '../../../shared/components/feed/feed.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FeedComponent],
   templateUrl: './user-profile.component.html',
 })
-export class UserProfileComponent {}
+export class UserProfileComponent implements OnInit {
+  slug: string = '';
+  isCurrentUserProfile$ = combineLatest({
+    currentUser: this.store.pipe(
+      select(selectCurrentUser),
+      filter((currentUser): currentUser is CurrentUserInterface =>
+        Boolean(currentUser)
+      )
+    ),
+    userProfile: this.store.pipe(
+      select(selectUserProfileData),
+      filter((userProfile): userProfile is UserProfileInterface =>
+        Boolean(userProfile)
+      )
+    ),
+  }).pipe(
+    map(
+      ({ currentUser, userProfile }) =>
+        currentUser.username === userProfile.username
+    )
+  );
+  data$ = combineLatest({
+    isLoading: this.store.select(selectIsLoading),
+    error: this.store.select(selectError),
+    userProfile: this.store.select(selectUserProfileData),
+    isCurrentUserProfile: this.isCurrentUserProfile$,
+  });
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private store: Store
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.slug = params['slug'];
+      this.fetchUserProfile();
+    });
+  }
+
+  fetchUserProfile(): void {
+    this.store.dispatch(userProfileActions.getUserProfile({ slug: this.slug }));
+  }
+
+  getApiUrl(): string {
+    const isFavorites = this.router.url.includes('favorites');
+    return isFavorites
+      ? `/articles?favorited=${this.slug}`
+      : `/articles?author=${this.slug}`;
+  }
+}
